a8223d13871bce3bf4b5e31cf822c63639921027
deuavt <connorjfrisby@gmail.com>
2026-02-23 17:13:46 +1100

    refactor marketmaker: initialise variance, improve structure, add docstrings

--- src/marketmaker.py
+++ src/marketmaker.py
@@ -6,88 +6,72 @@ class MarketMaker:
         self.book = book
         self.record = record
         self.steps = steps
-        
+
         self.risk_aversion = risk_aversion
         self.half_spread = half_spread
         self.volume = order_volume
         self.var_window_size = var_window_size
-        
-        # Last updated bid/ask ids and values.
+
+        # Last updated order ids and values.
         self.bid = {'id': None, 'volume': None, 'price': None}
         self.ask = {'id': None, 'volume': None, 'price': None}
 
         # Internal tracking values.
+        self.variance = None
+
         self.inventory = 0
         self.cash = 0
         self.mid_record = []
         self.pnl_record = []
         self.inv_record = []
-    
+
     def __reserve(self):
         """Return current reservation price."""
         s = self.book.get_midprice()
         if s is None:
             s = self.record[-1]['midprice']
 
-        q = self.inventory 
-        gam = self.risk_aversion
-        var = self.variance
-        T = self.steps
-        t = len(self.record)
+        q, gam, var, T, t = self.inventory, self.risk_aversion, self.variance, self.steps, len(self.record)
         # Avellaneda-Stoikov reservation price formula.
-        r = s - q * gam * var * (T - t) / T
-        return r
-    
+        return s - q * gam * var * (T - t) / T
+
     def __update_values(self):
+        """Update internal tracking values if there are any order fills."""
         book = self.book
-        # Update internal values for any bid fills.
-        bid_id = self.bid['id']
-        bid_volume = book.get_volume(bid_id)
-        if bid_volume is not None:
-            fill = self.bid['volume'] - bid_volume
-            self.cash -= fill * self.bid['price']
-            self.inventory += fill
-        elif bid_id is not None:
-            self.cash -= self.bid['volume'] * self.bid['price']
-            self.inventory += self.bid['volume']
-        # Update internal values for any ask fills.
-        ask_id = self.ask['id']
-        ask_volume = book.get_volume(ask_id)
-        if ask_volume is not None:
-            fill = self.ask['volume'] - ask_volume
-            self.cash += fill * self.ask['price']
-            self.inventory -= fill
-        elif ask_id is not None:
-            self.cash += self.ask['volume'] * self.ask['price']
-            self.inventory -= self.ask['volume']
-    
+        for direction, order in [(-1, self.ask), (1, self.bid)]:
+            order_id = order['id']
+            if order_id is None: 
+                continue
+
+            bid_volume = book.get_volume(order_id) or 0
+            fill = order['volume'] - bid_volume
+            self.cash -= direction * fill * order['price']
+            self.inventory += direction * fill
+
     def __update_records(self):
+        """Update the internal records for agent performance and midprice."""
         time = len(self.record)
         mid = self.book.get_midprice()
         if mid is None:
             mid = self.record[-1]['midprice']
+            
         pnl = self.cash + self.inventory * mid
         self.pnl_record.append((time, pnl))
         self.inv_record.append((time, self.inventory))
         self.mid_record.append((time, mid))
-    
+
     def __update_variance(self):
+        """Update the rolling variance based on the window size."""
         window_size = self.var_window_size
         if len(self.mid_record) >= window_size:
             window = self.mid_record[-window_size:]
-            mean_sum = 0
-            for x in window:
-                mean_sum += x[1]
-            mean = mean_sum / window_size
-
-            variance_sum = 0
-            for x in window:
-                variance_sum += (mean - x[1])**2
-            self.variance = variance_sum / window_size
+            mean = sum(mid[1] for mid in window) / window_size
+            self.variance = sum((mean - mid[1])**2 for mid in window) / window_size
         else:
             self.variance = None
 
     def __update_orders(self):
+        """Recalculate and recreate agent limit orders."""
         if self.variance is None:
             return
 
